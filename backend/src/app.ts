290f0660db83e6ae99c5985e0ae8552ee4fd542a
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2024-11-27 23:02:10 +0900

    셋업 중...

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,5 +6,9 @@ class Application {
     public initialize(): void {
         const app: Express = express();  // Express 애플리케이션 객체 생성
         const server: MySnsServer = new MySnsServer(app);  // MySnsServer 인스턴스를 생성하고 app 전달
+        server.start()
     }
 }
+
+const application: Application = new Application();  // Application 클래스의 인스턴스 생성
+application.initialize();  // 서버 초기화 메서드 호출
